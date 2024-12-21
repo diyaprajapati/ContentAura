313a8b50fb664033995b1b39e662125b823e6ff3
diyaprajapati <diyabprajapati2005@gmail.com>
2024-12-21 18:56:13 +0530

    made authentication backend

---
+++ cms-backend/src/main/java/com/ContentAura/cms_backend/auth/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.ContentAura.cms_backend.auth;
+
+import com.ContentAura.cms_backend.config.JwtService;
+import com.ContentAura.cms_backend.user.Role;
+import com.ContentAura.cms_backend.user.Role.*;
+import com.ContentAura.cms_backend.user.User;
+import com.ContentAura.cms_backend.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User existingUser = userRepository.findByEmail(request.getEmail()).orElse(null);
+        if (existingUser != null) {
+            throw new IllegalArgumentException("User already exists");
+        }
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
