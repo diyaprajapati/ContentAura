924c895c46a1b64120f5e98272d881424888e253
diyaprajapati <diyabprajapati2005@gmail.com>
2024-12-22 22:36:31 +0530

    added logout btn

--- cms-frontend/src/pages/Dashboard/Dashboard.tsx
+++ cms-frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
 export default function Dashboard() {
+  const navigate = useNavigate();
+  function handleLogout() {
+    localStorage.removeItem('token');
+    navigate('/auth');
+  }
+
   return (
     <div>
-      Hello
+      <Button onClick={handleLogout}>Logout</Button>
     </div>
   )
 }
