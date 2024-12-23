9c39f147df529e1a9de05ae20c722a058f5aef1d
diyaprajapati <diyabprajapati2005@gmail.com>
2024-12-23 22:00:59 +0530

    added sidebar

--- cms-frontend/src/App.tsx
+++ cms-frontend/src/App.tsx
@@ -3,18 +3,21 @@ import './index.css'
 import HomePage from './pages/Home/HomePage'
 import Authentication from './pages/Authentication/Authentication'
 import Dashboard from './pages/Dashboard/Dashboard'
+import Layout from './Layout'
 
 function App() {
   return (
     <>
-    {/* routers */}
-    <BrowserRouter>
-    <Routes>
-      <Route path='/dashboard' element={<Dashboard />}/>
-      <Route path='/auth' element={<Authentication/>}/>
-      <Route path="/"  element={<HomePage/>} />
-      </Routes>
-    </BrowserRouter>
+      {/* routers */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Layout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+          <Route path='/auth' element={<Authentication />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
