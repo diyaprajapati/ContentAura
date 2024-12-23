33af95244be5cc1cc269ee8b70f42b5dce53ccb0
diyaprajapati <diyabprajapati2005@gmail.com>
2024-12-23 15:42:01 +0530

    fix

--- cms-backend/src/main/java/com/ContentAura/cms_backend/auth/AuthenticationController.java
+++ cms-backend/src/main/java/com/ContentAura/cms_backend/auth/AuthenticationController.java
@@ -7,16 +7,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:5173", "https://content-aura.vercel.app/"})
 public class AuthenticationController {
     private final AuthenticationService service;
 
-    @CrossOrigin(origins = "http://localhost:5173")
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(service.register(request));
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
