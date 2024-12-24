817fa06964805a68c021556b499f81d4939ea9ef
diyaprajapati <diyabprajapati2005@gmail.com>
2024-12-24 15:35:29 +0530

    made dropdown for profile

--- cms-frontend/src/components/app-sidebar.tsx
+++ cms-frontend/src/components/app-sidebar.tsx
@@ -1,15 +1,21 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Calendar, ChevronUp, Home, Inbox, Search, Settings, User2 } from "lucide-react"
 
 import {
     Sidebar,
     SidebarContent,
+    SidebarFooter,
     SidebarGroup,
     SidebarGroupContent,
     SidebarGroupLabel,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    SidebarMenuSubItem,
 } from "@/components/ui/sidebar"
+import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { DropdownMenuContent, DropdownMenuItem } from "./ui/dropdown-menu"
+import { useNavigate } from "react-router-dom"
+import { Button } from "./ui/button"
 
 // Menu items.
 const items = [
@@ -47,12 +53,21 @@ const items = [
 ]
 
 export function AppSidebar() {
+
+    const navigate = useNavigate();
+    function handleLogout() {
+        localStorage.removeItem('token');
+        navigate('/auth');
+    }
+
     return (
         <Sidebar>
             <SidebarContent>
                 <SidebarGroup>
+                    {/* title */}
                     <SidebarGroupLabel>ContentAura</SidebarGroupLabel>
                     <SidebarGroupContent>
+                        {/* sidebar items */}
                         <SidebarMenu>
                             {items.map((item) => (
                                 <SidebarMenuItem key={item.title}>
@@ -68,6 +83,29 @@ export function AppSidebar() {
                     </SidebarGroupContent>
                 </SidebarGroup>
             </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuSubItem>
+                        {/* dropdown menu */}
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton>
+                                    <User2 /> Username
+                                    <ChevronUp className="ml-auto" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent side="top" className="w-[--radix-popper-anchor-width]">
+                                <DropdownMenuItem>
+                                    <span className="py-2">Account</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <Button onClick={handleLogout} className='bg-transparent hover:bg-transparent p-[-2] py-[-4] font-normal text-sm'>Logout </Button>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuSubItem>
+                </SidebarMenu>
+            </SidebarFooter>
         </Sidebar>
     )
 }
--- cms-frontend/src/pages/Dashboard/Dashboard.tsx
+++ cms-frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router-dom";
-
 export default function Dashboard() {
-  const navigate = useNavigate();
-  function handleLogout() {
-    localStorage.removeItem('token');
-    navigate('/auth');
-  }
 
   return (
     <div>
-      <Button onClick={handleLogout}>Logout</Button>
+      hello
     </div>
   )
 }
