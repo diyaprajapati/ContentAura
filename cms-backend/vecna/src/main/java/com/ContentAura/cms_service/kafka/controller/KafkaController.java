1e03df6c65789978760461a6bded5876b935eda7
diyaprajapati <diyabprajapati2005@gmail.com>
2025-02-16 14:40:11 +0530

    made vecna sending msg to kafka

---
+++ cms-backend/vecna/src/main/java/com/ContentAura/cms_service/kafka/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.ContentAura.cms_service.kafka.controller;
+
+import com.ContentAura.cms_service.kafka.producer.KafkaProducerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class KafkaController {
+    private final KafkaProducerService kafkaProducerService;
+
+    public KafkaController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
+    }
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        kafkaProducerService.sendMessage("contentaura-topic", message);
+        return "Message send: " + message;
+    }
+}
