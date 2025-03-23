7001c478af5e971a96b3e1aff7ff95d1a9e17fe0
diyaprajapati <diyabprajapati2005@gmail.com>
2025-03-23 19:06:43 +0530

    exceed limit

--- cms-backend/vecna/src/main/java/com/ContentAura/cms_service/auth/SignupRateLimiterService.java
+++ cms-backend/vecna/src/main/java/com/ContentAura/cms_service/auth/SignupRateLimiterService.java
@@ -14,7 +14,7 @@ public class SignupRateLimiterService {
     private final ConcurrentMap<String, Bucket> buckets = new ConcurrentHashMap<>();
 
     private Bucket createNewBucket() {
-        Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(5))); // 5 attempts per 5 minutes
+        Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(1))); // 5 attempts per 5 minutes
         return Bucket.builder().addLimit(limit).build();
     }
 
