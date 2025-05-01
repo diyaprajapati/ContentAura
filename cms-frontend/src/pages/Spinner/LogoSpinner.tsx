ca849ec764e7f5fca9fc1e6bdc468ff1b7b52514
diyaprajapati <diyabprajapati2005@gmail.com>
2025-05-01 21:23:38 +0530

    modify spinner

--- cms-frontend/src/pages/Spinner/LogoSpinner.tsx
+++ cms-frontend/src/pages/Spinner/LogoSpinner.tsx
@@ -2,7 +2,7 @@ import PulseLoader from "./PulseLoader";
 
 export default function LogoSpinner() {
     return (
-        <div className="flex flex-col items-center justify-center py-10 bg-transparent">
+        <div className="flex flex-col items-center justify-center py-10 bg-transparent h-full">
             <PulseLoader size="lg" color="brand" className="mb-4" />
         </div>
     )
